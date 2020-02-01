d7bffb7b485cc7f7d1641e9847b4ba7040a3d489
Uzor Mercy <mercy4hope@gmail.com>
2020-02-01 11:59:19 -1200

    fixed css navigation smooth scroll

--- js/main.js
+++ js/main.js
@@ -6,7 +6,5 @@ const navScroll = document.querySelector('.wg-navbar');
 window.addEventListener('scroll', (event)=>{
 	if(window.scrollY > 475){
 		navScroll.classList.add('nav-fixed')
-	}else if(window.scrollY < 120){
-		navScroll.classList.remove('nav-fixed');
 	}
 })
